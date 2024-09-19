620da9b671beaa0edb03149af60f8a99e6310cd7
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-09-19 01:21:58 -0400

    first commit

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -183,6 +183,7 @@ export default function Lab1() {
           id="wd-starship"
           width="400px"
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          alt="Starship gap image"
         />
         <br />
         Loading a local image:
