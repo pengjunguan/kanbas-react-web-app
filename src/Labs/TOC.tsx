62e51e7eb1f7880a70f8e062b222dda582985d59
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-09-19 11:50:55 -0400

    first commit

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,9 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li>
+            <a id="wd-github" href="https://github.com/pengjunguan/kanbas-react-web-app.git">GitHub</a>
+        </li>
       </ul>
     );
   }
