2917644d069cd814da208faa2a0dc55d90e9a71b
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-09-19 00:03:33 -0400

    first commit

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -9,7 +9,7 @@ export default function AssignmentEditor() {
         page should include the following: Your full name and section 
         Links to each of the lab assignments Link to the Kanbas application
         Links to all relevant source code repositories The Kanbas application 
-        should include a link to navigate back to the landing page. 123
+        should include a link to navigate back to the landing page.
         </textarea><br /><br />
         
         <table>
