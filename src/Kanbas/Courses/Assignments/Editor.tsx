a82a85c271f8ecb0282487a2be0fdde273c2ad44
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-10-05 22:09:14 -0400

    a3 JavaScript

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,154 +1,121 @@
 import { FaRegCalendarAlt } from "react-icons/fa";
+import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
+import * as db from "../../Database";  
+
 export default function AssignmentEditor() {
-    return (
-      <div id="wd-assignments-editor">
-        <label htmlFor="wd-name"><b>Assignment Name</b></label><br /><br />
-        <input id="wd-name" value="A1" className="form-control"/><br /><br />
-        <textarea id="wd-description" className="form-control" cols={45} rows={9}>
-        The assignment is available online Submit a link to the landing 
-        page of your Web application running on Netlify. The landing 
-        page should include the following: Your full name and section 
-        Links to each of the lab assignments Link to the Kanbas application
-        Links to all relevant source code repositories The Kanbas application 
-        should include a link to navigate back to the landing page.
-        </textarea><br /><br />
-        
-        <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td className="ps-3">
-            <input id="wd-points" className="form-control" value={100} />
-          </td>
-        </tr>
-        <br />
-        {/* Complete on your own */}
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-group">Assignment Group</label>
-          </td>
-          <td className="ps-3">
-            <select id="wd-group" className="form-select"> 
-              <option value="ASSIGNMENTS" selected>ASSIGNMENTS</option>
-              <option value="QUIZZES">QUIZZES</option>
-              <option value="PROJECT">PROJECT</option>
-              <option value="EXAM">EXAM</option>
-            </select>
-          </td>
-        </tr>
-        <br />
+  const { cid, aid } = useParams(); 
+  const assignments = db.assignments;
+  
+ 
+  const assignment = assignments.find((a) => a._id === aid);
 
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-display-grade-as">Display Grade as</label>
-          </td>
-          <td className="ps-3">
-            <select id="wd-display-grade-as" className="form-select">
-              <option selected value="Percentage">Percentage</option>
-              <option value="Letter Grades">Letter Grades</option>
-              <option value="GPA">GPA</option>
-            </select>
-          </td>
-        </tr>
-        <br />
+  const [assignmentData] = useState({
+    title: assignment ? "A1":"",
+    description: assignment ? "The assignment is available online" : "",
+    points: assignment ? 100 : 0,
+    dueDate: "May 13, 2024, 11:59 PM",
+    availableFrom: "May 6, 2024, 12:00 PM",
+  });
 
-        
-        <tr>
-  <td align="right" valign="top">
-    <label htmlFor="wd-submission-type">Submission Type</label>
-  </td>
-  <td className="ps-3">
-    <div className="border p-3"> 
-      <select id="wd-submission-type" className="form-select mb-3">
-        <option selected value="Online">Online</option>
-        <option value="In Class">In Class</option>
-      </select>
+  return (
+    <div id="wd-assignments-editor">
+      <label htmlFor="wd-name">Assignment Name</label><br /><br />
+      <input
+        id="wd-name"
+        className="form-control"
+        value={assignmentData.title}
+      /><br /><br />
 
-      <label><b>Online Entry Options</b></label>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="wd-text-entry" />
-        <label className="form-check-label" htmlFor="wd-text-entry">Text Entry</label>
-      </div>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="wd-website-url" checked />
-        <label className="form-check-label" htmlFor="wd-website-url">Website URL</label>
-      </div>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="wd-media-recordings" />
-        <label className="form-check-label" htmlFor="wd-media-recordings">Media Recordings</label>
-      </div>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="wd-student-annotation" />
-        <label className="form-check-label" htmlFor="wd-student-annotation">Student Annotation</label>
-      </div>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="wd-file-upload" />
-        <label className="form-check-label" htmlFor="wd-file-upload">File Uploads</label>
-      </div>
-    </div>
-  </td>
-</tr>
+      <textarea
+        id="wd-description"
+        className="form-control"
+        cols={45}
+        rows={9}
+        value={assignmentData.description}
+      /><br /><br />
 
-<br />
-
-<tr>
-  <td align="right" valign="top">
-    <label htmlFor="wd-assign-to" className="form-label">Assign to</label>
-  </td>
-  <td className="ps-3">
-    <div className="border p-3"> 
-      <div className="mb-3">
-        <label htmlFor="wd-assign-to"><b>Assign to</b></label>
-        <div className="form-control d-flex align-items-center" style={{ height: "auto" }}>
-          
-          <span className="badge bg-light text-dark px-3 py-2">Everyone 
-            <button type="button" className="btn-close ms-2" aria-label="Close"></button>
-          </span>
+      <div className="row justify-content-end">
+        <div className="col-md-9 d-flex align-items-top mb-3">
+          <label htmlFor="wd-points" className="me-3">Points</label>
+          <input
+            id="wd-points"
+            className="form-control"
+            value={assignmentData.points}
+            style={{ width: "100%" }} 
+          />
         </div>
-      </div>
 
-      <div className="mb-3">
-        <label htmlFor="wd-due-date" className="form-label"><b>Due</b></label>
-        <div className="input-group">
-          <input id="wd-due-date" type="text" className="form-control" value="May 13, 2024, 11:59 PM" />
-          <span className="input-group-text">
-      <FaRegCalendarAlt /> 
-    </span>
-        </div>
-      </div>
+        <div className="col-md-9 d-flex align-items-top mb-3">
+          <label htmlFor="wd-assign-to" className="me-3">Assign</label>
+          <div className="border rounded p-3" style={{ width: "100%" }}>
+            <div className="mb-3">
+              <label htmlFor="wd-assign-to" className="form-label"><b>Assign to</b></label>
+              <input
+                id="wd-assign-to"
+                className="form-control"
+              />
+            </div>
 
-      <div className="row">
-        <div className="col-md-6">
-          <label htmlFor="wd-available-from" className="form-label"><b>Available from</b></label>
-          <div className="input-group">
-            <input id="wd-available-from" type="text" className="form-control" value="May 6, 2024, 12:00 PM" />
-            <span className="input-group-text">
-      <FaRegCalendarAlt /> 
-    </span>
+            <div className="mb-3">
+              <label htmlFor="wd-due-date" className="form-label"><b>Due</b></label>
+              <div className="input-group">
+                <input
+                  id="wd-due-date"
+                  type="text"
+                  className="form-control"
+                  value={assignmentData.dueDate}
+                />
+                <span className="input-group-text">
+                  <FaRegCalendarAlt />
+                </span>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <label htmlFor="wd-available-from" className="form-label"><b>Available from</b></label>
+                <div className="input-group">
+                  <input
+                    id="wd-available-from"
+                    type="text"
+                    className="form-control"
+                    value={assignmentData.availableFrom}
+                  />
+                  <span className="input-group-text">
+                    <FaRegCalendarAlt />
+                  </span>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="wd-available-until" className="form-label"><b>Until</b></label>
+                <div className="input-group">
+                  <input
+                    id="wd-available-until"
+                    type="text"
+                    className="form-control"
+                  />
+                  <span className="input-group-text">
+                    <FaRegCalendarAlt />
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-md-6">
-  <label htmlFor="wd-available-until" className="form-label"><b>Until</b></label>
-  <div className="input-group">
-    <input id="wd-available-until" type="text" className="form-control" value="May 20, 2024, 12:00 PM" />
-    <span className="input-group-text">
-      <FaRegCalendarAlt /> 
-    </span>
-  </div>
-</div>
       </div>
-    </div>
-  </td>
-</tr>
 
-        </table>
-        <hr />
-        <div className="d-flex justify-content-end mt-3">
-        <button className="btn btn-secondary me-2">Cancel</button>
-        <button className="btn btn-danger">Save</button>
+      <hr />
+      <div className="d-flex justify-content-end mt-3">
+        <Link to={`/Kanbas/Courses/${cid}/Assignments`} className="btn btn-secondary me-2">
+          Cancel
+        </Link>
+        <Link to={`/Kanbas/Courses/${cid}/Assignments`} className="btn btn-danger">
+          Save
+        </Link>
       </div>
     </div>
-);}
+  );
+}
 
 
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,8 +4,14 @@ import AssignmentsControlButtons from "./AssignmentsControlButtons";
 import AssignControlButtons from "./AssignControlButtons";
 import { PiNotePencilLight } from "react-icons/pi";
 import { GoTriangleDown } from "react-icons/go";
+import { useParams } from "react-router";
+import * as db from "../../Database";  
 
 export default function Assignments() {
+  const { cid } = useParams();  
+  const assignments = db.assignments; 
+  const courseAssignments = assignments.filter((assignment) => assignment.course === cid);
+
   return (
     <div id="wd-assignments">
       <AssignmentsControls /><br/><br /><br />
@@ -19,59 +25,27 @@ export default function Assignments() {
           </div>
 
           <ul id="wd-assignment-list" className="list-group rounded-0">
-            {/* Assignment 1 */}
-            <li className="wd-assignment-list-item list-group-item d-flex justify-content-between align-items-center position-relative" style={{ borderLeft: '5px solid green', paddingLeft: '10px' }}>
-              <div className="d-flex align-items-center">
-                <BsGripVertical className="me-2 fs-3" />
-                <PiNotePencilLight className="me-2 fs-3" />
-                <div>
-                  <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123" style={{ color: 'black', textDecoration: 'none' }}>A1</a>
-                  <br />
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                  <br />
-                  <span><b>Due</b> May 13 at 11:59pm | 100 pts</span>
+            {/* Loop through filtered assignments */}
+            {courseAssignments.map((assignment) => (
+              <li key={assignment._id} className="wd-assignment-list-item list-group-item d-flex justify-content-between align-items-center position-relative" style={{ borderLeft: '5px solid green', paddingLeft: '10px' }}>
+                <div className="d-flex align-items-center">
+                  <BsGripVertical className="me-2 fs-3" />
+                  <PiNotePencilLight className="me-2 fs-3" />
+                  <div>
+                    <a className="wd-assignment-link" href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`} style={{ color: 'black', textDecoration: 'none' }}>
+                      {assignment.title}
+                    </a>
+                    <br />
+                    <span style={{ color: 'red' }}>Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
+                    <br />
+                    <span><b>Due</b> May 13 at 11:59pm | 100 pts</span>
+                  </div>
                 </div>
-              </div>
-              <div className="d-flex align-items-center">
-                <AssignmentsControlButtons />
-              </div>
-            </li>
-
-            {/* Assignment 2 */}
-            <li className="wd-assignment-list-item list-group-item d-flex justify-content-between align-items-center position-relative" style={{ borderLeft: '5px solid green', paddingLeft: '10px' }}>
-              <div className="d-flex align-items-center">
-                <BsGripVertical className="me-2 fs-3" />
-                <PiNotePencilLight className="me-2 fs-3" />
-                <div>
-                  <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123" style={{ color: 'black', textDecoration: 'none' }}>A2</a>
-                  <br />
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <b>Not available until</b> May 13 at 12:00am |
-                  <br />
-                  <span><b>Due</b> May 20 at 11:59pm | 100 pts</span>
+                <div className="d-flex align-items-center">
+                  <AssignmentsControlButtons />
                 </div>
-              </div>
-              <div className="d-flex align-items-center">
-                <AssignmentsControlButtons />
-              </div>
-            </li>
-
-            {/* Assignment 3 */}
-            <li className="wd-assignment-list-item list-group-item d-flex justify-content-between align-items-center position-relative" style={{ borderLeft: '5px solid green', paddingLeft: '10px' }}>
-              <div className="d-flex align-items-center">
-                <BsGripVertical className="me-2 fs-3" />
-                <PiNotePencilLight className="me-2 fs-3" />
-                <div>
-                  <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123" style={{ color: 'black', textDecoration: 'none' }}>A3</a>
-                  <br />
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <b>Not available until</b> May 20 at 12:00am |
-                  <br />
-                  <span><b>Due</b> May 27 at 11:59pm | 100 pts</span>
-                </div>
-              </div>
-              <div className="d-flex align-items-center">
-                <AssignmentsControlButtons />
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </li>
       </ul>
