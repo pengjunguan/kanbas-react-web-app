be0f4e223865f71f012abd96fe3bad38880fc282
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-10-08 00:33:32 -0400

    a3 JavaScript

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -76,14 +76,7 @@ export default function Lab3() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
         vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
      </Highlight>
-
-     <AddPathParameters/>
      <PathParameters/>
-
-    
-    
-  
-
     </div>
   );
 }
