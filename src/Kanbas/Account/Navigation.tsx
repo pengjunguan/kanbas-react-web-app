c2a4db6568e59ea4d9b1b952e4372bbbd1befb64
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-11-04 01:56:31 -0500

    a4 Redux

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,10 +1,10 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./Navigation.css"; 
 
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  /* const { currentUser } = useSelector((state: any) => state.accountReducer); */
   /* const links = currentUser ? ["Profile"] : ["Signin", "Signup"]; */
   /* const { pathname } = useLocation(); */
 
