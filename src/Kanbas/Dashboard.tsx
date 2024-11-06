526cbed8a0d5b4a83419eee499073715a5d73a12
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-11-05 19:18:54 -0500

    a4 Redux

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { enrollCourse, unenrollCourse, loadEnrollments } from "./EnrollmentReducer";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Dashboard({
   courses,
